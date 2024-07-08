a49a099fdec0bac4b910988363a294db88614597
Dan9el8 <mutetemifloice@gmail.com>
2024-07-08 17:13:38 +0300

    init1

--- user_auth_org/user_auth_org/settings.py
+++ user_auth_org/user_auth_org/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-96^i%a$dd46kdowegy!i&jn@z%9lqu!yx^38k5cf+$mw#ygwb1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = ["a871-105-161-1-93.ngrok-free.app"]
 
 
 # Application definition
