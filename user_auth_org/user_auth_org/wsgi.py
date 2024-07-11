e51665b2ae4dd4a31ffb613c08b791910a31f6c7
Dan9el8 <mutetemifloice@gmail.com>
2024-07-11 15:14:05 +0300

    init

--- user_auth_org/user_auth_org/wsgi.py
+++ user_auth_org/user_auth_org/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_auth_org.settings')
 
 application = get_wsgi_application()
-
-app = application
